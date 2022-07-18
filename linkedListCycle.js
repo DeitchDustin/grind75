7b18c0e1b07111e326d4aeff180379511624cfdc
Dustin Deitch <DeitchDustin@gmail.com>
2022-07-18 13:42:03 -0700

    added comments

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -4,17 +4,22 @@
 // E - if  only linked list head then true
 
 var hasCycle = function(head) {
+  //Create slow / fast var
   var tortoise = head;
   var hare = head;
 
+  // while fast exists and has a next value
   while (hare && hare.next) {
+    //set slow to next
          tortoise = tortoise.next;
+         //set fast to next twice
          hare = hare.next.next;
 
+         // if fast ever equals slow return true
       if (tortoise === hare) {
           return true;
           }
          }
-
+//else return false
   return false;
 };
